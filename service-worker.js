3e0cd1eb9f288d1b05bc66b641467b23433c5eeb
ilhamadhim <ilhamm179@gmail.com>
2020-07-22 21:15:54 +0700

    Cache Optimization

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,40 @@
-const CACHE_NAME = "firstpwa-v1";
+const CACHE_NAME = "premier-league-soccer";
 var urlsToCache = [
   "/",
+  "/css/style.css",
   "/nav.html",
   "/index.html",
-  "/article.html",
+  "/team.html",
   "/pages/home.html",
   "/pages/about.html",
   "/pages/contact.html",
   "/css/materialize.min.css",
   "/js/materialize.min.js",
-  "/manifest.json",
   "/js/nav.js",
   "/js/api.js",
-  "/icon.png"
+  "/js/idb.js",
+  "/js/db.js",
+  "/manifest.json",
+  "/iconsplash_256.png",
+  "/iconsplash_512.png",
+  "assets/offline.png"
 ];
 
-self.addEventListener("install", function(event) {
+self.addEventListener("install", function (event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
+    caches.open(CACHE_NAME).then(function (cache) {
       return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener("fetch", function(event) {
-  var base_url = "https://readerapi.codepolitan.com/";
+self.addEventListener("fetch", function (event) {
+  var base_url = "https://api.football-data.org/v2/";
 
   if (event.request.url.indexOf(base_url) > -1) {
     event.respondWith(
-      caches.open(CACHE_NAME).then(function(cache) {
-        return fetch(event.request).then(function(response) {
+      caches.open(CACHE_NAME).then(function (cache) {
+        return fetch(event.request).then(function (response) {
           cache.put(event.request.url, response.clone());
           return response;
         })
@@ -37,18 +42,18 @@ self.addEventListener("fetch", function(event) {
     );
   } else {
     event.respondWith(
-      caches.match(event.request, { ignoreSearch: true }).then(function(response) {
-        return response || fetch (event.request);
+      caches.match(event.request, { ignoreSearch: true }).then(function (response) {
+        return response || fetch(event.request);
       })
     )
   }
 });
 
-self.addEventListener("activate", function(event) {
+self.addEventListener("activate", function (event) {
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
+    caches.keys().then(function (cacheNames) {
       return Promise.all(
-        cacheNames.map(function(cacheName) {
+        cacheNames.map(function (cacheName) {
           if (cacheName != CACHE_NAME) {
             console.log("ServiceWorker: cache " + cacheName + " dihapus");
             return caches.delete(cacheName);
