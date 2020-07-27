b928a79374552510e189ddfa4eefac04e18e6150
ilhamadhim <ilhamm179@gmail.com>
2020-07-27 12:30:40 +0700

    Fix layout teams and saved

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 let pathImagesTopScorer = ["../assets/first-top-scorer.png", "../assets/second-top-scorer.png", "../assets/third-top-scorer.png"]
 
-
 let renderStandings = () => {
     showLoader();
     let standings = getStandings();
@@ -107,49 +106,66 @@ let renderTeams = () => {
 
         if (page === 'saved') {
             teamHTML = `<h3> My Favorite Teams </h3>`;
+
             // If the user added any favorite team
             data.length > 0 ?
                 data.forEach(club => {
                     teamHTML += `
-                        <div class="col s12 m6 l4" data-aos="fade-left">
-                            <div class="card small center-align">
-                                <div class = "card-content grey lighten-4" style="height:100px"></div>
-                                <div class="card-content" style="height:150px">
-                                    <a href="./team.html?id=${club.id}">
-                                        <div class="top-space" style="font-size:1.2em;">${club.name}</div>
-                                    </a>
-                                </div>
-                                <img src="${club.crestUrl}" alt="${club.name} logo" class="team-preview "/>
-                                <div class="card-action right-align">
-                                    <a class="waves-effect waves-light btn-small red" onclick="deleteTeamListener(${club.id})"><i class="material-icons left">delete</i>Delete</a>
-                                </div>
+
+                    <div class="col s12 m6 l4" data-aos="fade-left">
+                        <div class="card small team-card center-align">
+                            <a href="./team.html?id=${club.id}" class="waves-effect waves-light">
+                            <div class = "card-image" style="height:150px">
+                                <img src="../assets/${club.name}-stadium.jpg" alt="${club.name}-stadium" height=150> 
+                            </div>
+                            <div class="card-content grey lighten-4" style="height:150px;">
+                                <br>
+                                <div class="team-name" style="font-size:1.2em;color:black">${club.name}</div>
+                            </div>                    
+                                <img src="${club.crestUrl}" alt="${club.name} logo" class="team-preview" />
+                            </a>
+                            <div class="card-action right-align">
+                                <a class="waves-effect waves-light btn-small red" onclick="deleteTeamListener(${club.id})"><i class="material-icons left">delete</i>Delete</a>
                             </div>
                         </div>
-                    `;
+                    </div>`;
+
                 })
                 // If the user hasn't added any fav team
-                : teamHTML = `<div class="container" style="height:80vh"> 
-                                <div class="center">
-                                <img src="../assets/soccer-standing.png" width=128 alt="no-fav-team"> <br>
-                                    No favorite teams added
+                : teamHTML = `< div class="container" style = "height:80vh" >
+                        <div class="center">
+                            <img src="../assets/soccer-standing.png" width=128 alt="no-fav-team"> <br>
+                                No favorite teams added
                                 </div>
-                            </div>`;
+                        </>`;
         } else {
+            // Sort Team name alphabetically
+            data.standings[0].table.sort((a, b) => {
+                var nameA = a.team.name.toLowerCase(), nameB = b.team.name.toLowerCase()
+                if (nameA < nameB) //sort string ascending
+                    return -1
+                if (nameA > nameB)
+                    return 1
+                return 0 //default return value (no sorting)
+            });
+
             data.standings[0].table.forEach(club => {
                 teamHTML += `
-                <div class="col s12 m6 l4" data-aos="fade-left">
-                    <div class="card center-align" style="height:300px">
-                        <div class="card-content grey lighten-4" style="height:100px"></div>
-                        <div class="card-content" style="height:150px">
-                            <a href="./team.html?id=${club.team.id}">
-                                <br>
-                                <div class="top-space" style="font-size:1.2em">${club.team.name}</div>
-                            </a>
-                        </div>
-
-                        <img src="${club.team.crestUrl}" alt="${club.team.name} logo" class="team-preview " />
-                    </div>
-                </div>`;
+                        <div class="col s12 m6 l4" data - aos="fade-left" >
+                            <div class="card team-card center-align" style="height:300px">
+                                <a href="./team.html?id=${club.team.id}" class="waves-effect waves-light">
+                                <div class="card-image" style="height:150px">
+                                    <img src="../assets/${club.team.name}-stadium.jpg" alt="${club.team.name}-stadium"
+                                    height=150 style="z-index:-1">
+                                </div>
+                                <div class="card-content grey lighten-4" style="height:150px;z-index:0">
+                                    <br>
+                                        <div class="team-name" style="font-size:1.2em;color:black">${club.team.name}</div>
+                                </div>
+                                    <img src="${club.team.crestUrl}" alt="${club.team.name} logo" class="team-preview" />
+                                </a>
+                            </div>
+                        </div>`;
             });
         }
         // Sisipkan komponen card ke dalam elemen dengan id #content
@@ -166,20 +182,20 @@ let renderTeamById = () => {
     team.then(data => {
         // Menyusun komponen card artikel secara dinamis
         let teamHTML = `
-        <div class="card top-space">
-            <div class="row card-content">
-                <div class="col s12 m4 l4 center-align">
-                    <img src="${data.crestUrl}" alt="${data.shortName}" width=128px height=128px/>
-                </div>
-            <div class="col s12 m8 l8">
-                <div class="center-align" style="padding:8px"> <b> ${data.name} </b> </div>
-                <i class="material-icons" style="font-size:.8rem"> sports_soccer </i>  ${data.venue} <br><br>
-                    <i class="material-icons" style="font-size:.8rem"> location_on </i>  ${data.address}  <br><br>
-                        <i class="material-icons" style="font-size:.8rem"> phone </i> ${data.phone !== null ? data.phone : 'No contact'}  <br><br>
-                            <b> Official Website : </b> <a href="${data.website}"> ${data.website}  </a> 
+                        < div class="card top-space" >
+                            <div class="row card-content">
+                                <div class="col s12 m4 l4 center-align">
+                                    <img src="${data.crestUrl}" alt="${data.shortName}" width=128px height=128px/>
+                    <div class="center-align" style="padding:8px"> <b> ${data.name} </b> </div>
+                                </div>
+                                <div class="col s12 m8 l8">
+                                    <i class="material-icons" style="font-size:.8rem"> sports_soccer </i>  ${data.venue} <br><br>
+                                        <i class="material-icons" style="font-size:.8rem"> location_on </i>  ${data.address}  <br><br>
+                                            <i class="material-icons" style="font-size:.8rem"> phone </i> ${data.phone !== null ? data.phone : 'No contact'}  <br><br>
+                                                <b> Official Website : </b> <a href="${data.website}"> ${data.website}  </a> 
                     </div>
                 </div>
-            </div>
+            </>
         </div>
         `;
         // Sisipkan komponen card ke dalam elemen dengan id #content
@@ -230,18 +246,18 @@ let renderMatches = () => {
             console.log(data);
             matchHTML +=
                 `<div class="container">
-                        <div class="row card">
-                            <div class="card-content center-align">
-                                <div class="col s5"> ${data.homeTeam.name} </div>
-                                <div class="col s1" style="padding:0"> VS </div>
-                                <div class="col s5"> ${data.awayTeam.name} </div>
-                            </div>
-                            <div class="card-content right-align">
-                                <!-- Only get the date -->
+                                            <div class="row card">
+                                                <div class="card-content center-align">
+                                                    <div class="col s5"> ${data.homeTeam.name} </div>
+                                                    <div class="col s1" style="padding:0"> VS </div>
+                                                    <div class="col s5"> ${data.awayTeam.name} </div>
+                                                </div>
+                                                <div class="card-content right-align">
+                                                    <!-- Only get the date -->
                             ${data.utcDate.substr(0, 10)}
-                            </div>
-                        </div>
-                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
             `
         });
         // Sisipkan komponen card ke dalam elemen dengan id #next-match-list
